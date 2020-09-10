268b824b4b598167505106af667983358029c507
Sterling <sterlingsgamble@gmail.com>
2020-09-10 13:49:36 -0700

    flix part 2

---
+++ flix/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  flix
+//
+//  Created by Sterling Gamble on 9/10/20.
+//  Copyright © 2020 Codepath. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
